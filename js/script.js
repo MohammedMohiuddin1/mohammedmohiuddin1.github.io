7cccea42d8cff07865629ec9cbe62aadf628efaf
MohammedMohiuddin1 <mohammedm78692@gmail.com>
2025-01-11 19:23:46 -0800

    fixed mobile display potentially?

--- js/script.js
+++ js/script.js
@@ -2,52 +2,56 @@
 var typed = new Typed(".typing", {
     strings: ["Software Engineer", "Full Stack Developer", "Cloud Developer", "DSA Tutor"],
     typeSpeed: 100,
-    BackSpeed: 60,
+    backSpeed: 60,
     loop: true
-})
+});
+
 /* ==================================== Aside ==================================== */
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length
+    totalNavList = navList.length;
+
 for (let i = 0; i < totalNavList; i++) {
-    console.log(navList[i])
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function () {
         for (let j = 0; j < totalNavList; j++) {
             navList[j].querySelector("a").classList.remove("active");
         }
-        this.classList.add("active")
-        showSection(this)
-    })
-
+        this.classList.add("active");
+        showSection(this);
+        if (window.innerWidth < 768) {
+            aside.classList.remove("open"); /* Close sidebar on mobile after clicking a link */
+        }
+    });
 }
-function showSection(element)
-{
+
+function showSection(element) {
     const target = element.getAttribute("href").split("#")[1];
-    document.querySelector('#' + target).classList.add("active")
+    document.querySelector('#' + target).classList.add("active");
 }
-const navTogglerBtn = document.querySelector(".nav-toggler"),
-aside = document.querySelector(".aside");
-navTogglerBtn.addEventListener("click", () =>
-{
-    asideSectionTogglerBtn();
-})
 
-function asideSectionTogglerBtn()
-{
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+});
+
+function asideSectionTogglerBtn() {
     aside.classList.toggle("open");
     navTogglerBtn.classList.toggle("open");
 }
+
 function downloadCV() {
     var link = document.createElement('a');
-    link.href = 'files/muz-resume.pdf'
+    link.href = 'files/muz-resume.pdf';
     link.download = 'muz-resume.pdf';
     link.click();
 }
 
 document.getElementById('downloadCvBtn').addEventListener('click', downloadCV);
 
-/* for active color changing of sections while scrolling */
+/* For active color changing of sections while scrolling */
 document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll(".section");
     const navLinks = document.querySelectorAll(".nav a");
